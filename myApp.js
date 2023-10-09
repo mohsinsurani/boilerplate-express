568cb6627727acd479b52784a3606f2414ce6cb5
Mohsin <mohsinmax786@gmail.com>
2023-10-09 23:25:03 +0100

    splace gone

--- myApp.js
+++ myApp.js
@@ -2,38 +2,4 @@ let express = require('express');
 let app = express();
 
 console.log("Hello World");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
